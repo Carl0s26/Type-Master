43c6bca2a17fafb389328ac47e4e7c6b5d49c580
Carl0s26 <Carloskepp26@gmail.com>
2025-02-12 13:57:26 -0400

    Started the function structure
    Added some functions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import flet as ft
 
+
+
 def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.LIGHT
     page.title = 'Game & Slice'
@@ -9,14 +11,30 @@ def main(page: ft.Page):
     page.scroll = True
     page.window.resizable = False
 
+    def on_keyboard(e: ft.KeyboardEvent):
+        print(e.key)
+
+        page.update()
+
+    page.on_keyboard_event = on_keyboard
+
+    def colorUpdater(label,value):
+        pass
+
+    def acurracyCalculator(label,correctWords,WordsShown):
+        pass
+
+    
+
     words = [""]
-    title = ft.Text(label="Hello Word")
+    title = ft.Text(value="Hello Word")
     userInput = ft.TextField()
-    presicionLabel = ft.Text()
-    wordLabel = ft.Text(label="Current Word")
+    acurracyLabel = ft.Text()
+    wordLabel = ft.Text(value="Current Word")
 
 
 
-    page.add(title,userInput,presicionLabel,wordLabel)
+    page.bgcolor = '#c8eed8'
+    page.add(title,userInput,acurracyLabel,wordLabel)
     page.update()
 ft.app(target=main, assets_dir="assets")
